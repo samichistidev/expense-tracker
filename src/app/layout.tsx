a2f20b80e53d083d0aa4c8c93aa04981cf85e6ac
mdsamichisty <sami.dev.fed@gmail.com>
2025-07-05 13:19:43 +0600

    updated meta keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
   title: "Expense Tracker by Sami Chisti • Front End Developer",
   description:
     "A Expense Tracker featuring dark/light mode toggle, multi-currency selection, date-stamped transactions, and localStorage persistence. Easily monitor and manage your income and expenses.",
+  keywords:
+    "expense tracker, personal finance app, React, Tailwind CSS, dark mode toggle, multi-currency support, date picker, tip of the day, localStorage persistence, calendar popover, budgeting, financial dashboard",
 };
 
 export default function RootLayout({
